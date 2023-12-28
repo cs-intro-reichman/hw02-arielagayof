d55e62d5319dd8e0c82f9cdc5d09226928c942be
Ariel Agayof <arielagayof@Ariels-MacBook-Air.local>
2023-12-28 19:58:44 +0200

    4th upload Dec28

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -5,11 +5,12 @@ public class DamkaBoard {
 	public static void main(String[] args) {
 		int n = Integer.parseInt(args[0]);
 		for (int i = 1; i < (n + 1); i++) {
-			if ((i % 2) == 0) {
-				System.out.print(" ");
-				}
 			for (int j = 1; j < (n + 1); j++) {
+				if ((i % 2) == 0) {
+				System.out.print(" *");
+				} else {
 				System.out.print("* ");
+			}
 			}
 			System.out.println();
 		}
